f0aa7652a79fce1e38c105264fc1f916381e0699
Victor Pozdnyakov <victorius152@gmail.com>
2022-12-13 00:16:11 +0600

    Associate navController with MainActivity
    Add ViewBinding to project and use in MainActivity

--- app/src/main/java/com/lemon/regioncodeskg/MainActivity.kt
+++ app/src/main/java/com/lemon/regioncodeskg/MainActivity.kt
@@ -2,16 +2,43 @@ package com.lemon.regioncodeskg
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.lemon.regioncodeskg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     private val navController by lazy {
-        Navigation.findNavController(this, R.id.nav_host_fragment)
+        Navigation.findNavController(this, R.id.navHostFragment)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupDrawerLayout()
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.root)
+    }
+
+    private fun setupDrawerLayout() {
+        binding.navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.root)
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
