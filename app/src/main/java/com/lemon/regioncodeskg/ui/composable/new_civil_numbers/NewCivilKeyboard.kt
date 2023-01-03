a49b1d94ac6cccaa607ace6b4c1ccd321ef6241d
Victor Pozdnyakov <victorius152@gmail.com>
2023-01-03 18:01:55 +0600

    Create composable custom keyboard for civil nums

--- app/src/main/java/com/lemon/regioncodeskg/ui/composable/new_civil_numbers/NewCivilKeyboard.kt
+++ app/src/main/java/com/lemon/regioncodeskg/ui/composable/new_civil_numbers/NewCivilKeyboard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -15,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lemon.regioncodeskg.R
 import androidx.compose.ui.tooling.preview.Preview
+import com.lemon.regioncodeskg.ui.composable.common.KeyboardItem
 
 @Preview
 @Composable
@@ -25,13 +27,14 @@ internal fun ContentViewPreview() {
 @Composable
 internal fun ContentView() {
 
-    Column(modifier = Modifier
-        .padding(vertical = 10.dp, horizontal = 30.dp)
-        .wrapContentHeight()
-        .width(100.dp)
-        .background(colorResource(id = R.color.white))) {
+    Column(modifier = Modifier.fillMaxSize()) {
 
-        Column(modifier = Modifier, verticalArrangement = Arrangement.Center) {
+        Column(modifier = Modifier
+            .padding(vertical = 10.dp, horizontal = 30.dp)
+            .wrapContentHeight()
+            .width(100.dp)
+            .background(colorResource(id = R.color.white)),
+            verticalArrangement = Arrangement.Center) {
 
             Text(modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
@@ -54,6 +57,27 @@ internal fun ContentView() {
 
         }
 
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(vertical = 10.dp, horizontal = 30.dp)
+            .wrapContentSize()
+            .fillMaxWidth(),
+            verticalArrangement = Arrangement.Bottom) {
+            var j = 0
+            for (i in 1..3) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    while (j < i * 3) {
+                        j += 1
+                        Column(Modifier.weight(1f)
+                            .padding(horizontal = 20.dp, vertical = 10.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally) {
+
+                            KeyboardItem(j.toString())
+                        }
+                    }
+                }
+            }
+        }
 
     }
 
