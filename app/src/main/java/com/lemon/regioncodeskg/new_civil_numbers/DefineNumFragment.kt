c800090f080d7c23c74dc09fa049e7e0d1284fba
Victor Pozdnyakov <victorius152@gmail.com>
2023-03-06 22:42:49 +0600

    Bind store to view

--- app/src/main/java/com/lemon/regioncodeskg/new_civil_numbers/DefineNumFragment.kt
+++ app/src/main/java/com/lemon/regioncodeskg/new_civil_numbers/DefineNumFragment.kt
@@ -9,13 +9,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.arkivanov.mvikotlin.core.binder.Binder
+import com.arkivanov.mvikotlin.extensions.coroutines.bind
+import com.arkivanov.mvikotlin.extensions.coroutines.states
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 import com.lemon.regioncodeskg.R
 import com.lemon.regioncodeskg.new_civil_numbers.composable.ContentView
+import com.lemon.regioncodeskg.new_civil_numbers.store.NewCivilStore
+import com.lemon.regioncodeskg.new_civil_numbers.store.NewCivilStoreFactory
+import kotlinx.coroutines.flow.transform
 
 class DefineNumFragment : Fragment(R.layout.fragment_define_num) {
 
     private val viewModelFactory = NewCivilViewModel.factory
-    private lateinit var viewModel: NewCivilViewModel;
+    private lateinit var viewModel: NewCivilViewModel
+    private val store = NewCivilStoreFactory.create(DefaultStoreFactory())
+    private var binder: Binder? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +44,19 @@ class DefineNumFragment : Fragment(R.layout.fragment_define_num) {
                 }
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binder = bind(viewModel.viewModelScope.coroutineContext) {
+            store.states.transform<NewCivilStore.State, ViewModelState> { stateToModel } bindTo viewModel::onState
+            viewModel.uiEvents.transform<UiEvent, NewCivilStore.Intent> { eventToIntent } bindTo store
+        }
+        binder?.start()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binder?.stop()
     }
 
 }
