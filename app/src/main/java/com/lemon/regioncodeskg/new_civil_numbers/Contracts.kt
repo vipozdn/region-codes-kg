77d7fbc82812e8e66b0bd2a20001247401178052
Victor Pozdnyakov <victorius152@gmail.com>
2023-03-06 21:35:54 +0600

    Create NewCivilBinder

--- app/src/main/java/com/lemon/regioncodeskg/new_civil_numbers/Contracts.kt
+++ app/src/main/java/com/lemon/regioncodeskg/new_civil_numbers/Contracts.kt
@@ -1,10 +1,10 @@
 package com.lemon.regioncodeskg.new_civil_numbers
 
 sealed interface UiEvent {
-
+    data class DefineNum(val newCivilIds: List<String>) : UiEvent
 }
 
-data class ViewModelState(val region: String = "") {
+data class ViewModelState(val regionCodeResId: Int = -1) {
 
     companion object {
         val INITIAL = ViewModelState()
--- app/src/main/java/com/lemon/regioncodeskg/new_civil_numbers/NewCivilBinder.kt
+++ app/src/main/java/com/lemon/regioncodeskg/new_civil_numbers/NewCivilBinder.kt
@@ -1,4 +1,17 @@
 package com.lemon.regioncodeskg.new_civil_numbers
 
-class NewCivilBinder {
+import com.lemon.regioncodeskg.new_civil_numbers.store.NewCivilStore
+
+internal val stateToModel: NewCivilStore.State.() -> ViewModelState = {
+    when (this) {
+        is NewCivilStore.State.DefineNumOutput -> {
+            ViewModelState(regionCodeResId = this.outputStrResId)
+        }
+    }
+}
+
+internal val eventToIntent: UiEvent.() -> NewCivilStore.Intent = {
+    when (this) {
+        is UiEvent.DefineNum -> NewCivilStore.Intent.DefineNum(this.newCivilIds)
+    }
 }
